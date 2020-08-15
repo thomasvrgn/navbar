d9e12832fd96bbc9ee7dd05407b85d32f93d84f8
NessMC <contact@nessmc.fr>
2020-08-15 19:11:55 +0200

    💄 Navbar dropdown UI added.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const navbar = document.getElementById('navbarExample'),
   button = document.getElementById('navbarButton'),
-  container = document.getElementById('navbarContainer')
+  container = document.getElementById('navbarContainer'),
+  dropdown = navbar.querySelectorAll('.navbarDropdown')
 let switched = false
 
 button.addEventListener('click', function (e) {
@@ -8,6 +9,15 @@ button.addEventListener('click', function (e) {
   if (switched) container.classList.remove('hidden')
   else container.classList.add('hidden')
 })
+
+for (const dropdownElement of dropdown) {
+  dropdownElement.parentNode.addEventListener('click', function (e) {
+    if (dropdownElement.classList.contains('hidden'))
+      dropdownElement.classList.remove('hidden')
+    else
+      dropdownElement.classList.add('hidden')
+  })
+}
 
 window.addEventListener('resize', function (e) {
   if (window.matchMedia('(min-width: 768px)').matches) {
